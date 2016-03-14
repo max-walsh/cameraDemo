eae40864244fc2d273341be0f171b471a3472e6d
Katie Kuenster <katiekuenster@katiekustersair.dhcp.nd.edu>
2016-03-14 16:15:00 -0400

    Can now access the camera roll.

--- cameraDemo/ViewController.swift
+++ cameraDemo/ViewController.swift
@@ -7,13 +7,31 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     var newMedia : Bool?
     
+    @IBAction func useCamera(sender: AnyObject) {
+    }
     
+    
+    // button for accesing the camera roll
+    @IBAction func useCameraRoll(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerControllerSourceType.SavedPhotosAlbum) {
+                let imagePicker = UIImagePickerController()
+                
+                imagePicker.delegate = self
+                imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+                imagePicker.mediaTypes = [kUTTypeImage as NSString as String]
+                imagePicker.allowsEditing = false
+                self.presentViewController(imagePicker, animated: true, completion: nil)
+                newMedia = false // no picture was taken, leaves imageview the same
+        }
+    }
     
     
     override func viewDidLoad() {
